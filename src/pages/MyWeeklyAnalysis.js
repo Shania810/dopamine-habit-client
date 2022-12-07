3172e4a8b7c1389a2661bcdc1f372c7ee478f7f8
Anneliese Kappey <anneliesekappey@gmail.com>
2022-12-07 10:42:32 +0100

    one minor fix

--- src/pages/MyWeeklyAnalysis.js
+++ src/pages/MyWeeklyAnalysis.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const MyWeeklyAnalysis = () => {
   return (
     <div>
-      <span> here's your graph for the week </span>
+      <p> here's your graph for the week </p>
     </div>
   )
 }
